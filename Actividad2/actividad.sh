aef616bc121db19de95ca1ccf46d592249c2501d
Sharon Tagual <sharotago@gmail.com>
2024-07-28 23:49:54 -0600

    actividad2

---
+++ Actividad2/actividad.sh
@@ -0,0 +1,27 @@
+#Actividad2 SISTEMAS OPERATIVOS 1
+
+# Definiendo mi variable con mi usuario de git
+GITHUB_USER=SharonT2
+
+# llamando api de git y pasando el usuario
+consultaAPI=$(curl -s "https://api.github.com/users/$GITHUB_USER")
+
+echo "Respuesta de la API: $consultaAPI"
+
+# Extraer datos del JSON
+identificador=$(echo $consultaAPI | jq -r '.id')
+CREATED_AT=$(echo $consultaAPI | jq -r '.created_at')
+
+# mensaje correspondiente
+mensajeOut="Hola $GITHUB_USER. User ID: $identificador. Cuenta fue creada el: $CREATED_AT."
+
+# Salida del mensaje
+echo $mensajeOut
+
+# Crear directorio para el log
+date=$(date +%Y-%m-%d)
+directorio="./$date"
+mkdir -p $directorio
+
+# Escribir mensaje en el log
+echo $mensajeOut >> "$directorio/saludos.log"
